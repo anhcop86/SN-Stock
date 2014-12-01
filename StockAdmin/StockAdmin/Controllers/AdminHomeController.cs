637954dd64ac249449962fd6aa38acadd358136c
AnhCopNe <n.hieu86@gmail.com>
2014-12-01 17:29:15 +0700

    add StockAdmin.csproj

---
+++ StockAdmin/StockAdmin/Controllers/AdminHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockAdmin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        //
+        // GET: /AdminHome/
+
+        public ActionResult AdminHomeView()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ StockAdmin/StockAdmin/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult didLogin(string txtUserName, string txtPassword)
+        {
+            return RedirectToAction("AdminHomeView", "AdminHome");
+        }
+    }
+}
