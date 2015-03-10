bff0679d9c1261d611ca85872c6c5e085ea45b34
AnhCopNe <n.hieu86@gmail.com>
2015-03-10 17:50:39 +0700

    TOi uu script

--- PhimHang.vn- layout 4/PhimHang.vn/App_Start/BundleConfig.cs
+++ PhimHang.vn- layout 4/PhimHang.vn/App_Start/BundleConfig.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web;
 using System.Web.Optimization;
-
+using BundleTransformer.Core.Builders;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Resolvers;
+using BundleTransformer.Core.Transformers;
 namespace PhimHang
 {
     public class BundleConfig
@@ -8,34 +11,53 @@ namespace PhimHang
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            
+            //This setting is used when if you have specfied the path Using System.web.Optimization.bundle.Cdnpath then it will try to fetch data from there first
+            bundles.UseCdn = true;
+            //NullBuilder class is responsible for prevention of early applying of the item transformations and combining of code.
+            var nullBuilder = new NullBuilder();
+            //StyleTransformer and ScriptTransformer classes produce processing of stylesheets and scripts.
+            var styleTransformer = new StyleTransformer();
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            var scriptTransformer = new ScriptTransformer();
+            //NullOrderer class disables the built-in sorting mechanism and save assets sorted in the order they are declared.
+            var nullOrderer = new NullOrderer();
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
-
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/standard").Include(
+            bundles.Add(new ScriptBundle("~/bundles/standardCoreRT").Include(
+                                        //<!--Jquery standard-->
+                                        "~/Scripts/jquery-2.1.1.min.js",
+                                        "~/ThemeBlue/js/jquery/jquery.js",
+                                         
+                                        "~/ThemeBlue/js/jquery/jquery-ui.js",
                                         //<!--For realtime UI-->
                                         "~/Scripts/knockout-3.2.0.js",
-                                        "~/Scripts/knockout.mapping-latest.js",
-                                                    //<!---->
+                                        "~/Scripts/knockout.mapping-latest.js",                                                    
                                         //<!--For realtime core-->
                                         "~/Scripts/jquery.signalR-2.1.2.min.js",
-                                        "~/ThemeBlue/js/Extention.js"
+                                        "~/ThemeBlue/js/Extention.js", // notification of top bar
+                                        "~/ThemeBlue/js/SignalHubProxy.js"
                                         ));
+
+            bundles.Add(new StyleBundle("~/bundles/StyleStandard").Include(
+                    "~/ThemeBlue/css/jquery-ui/jquery-ui.css",
+                      "~/ThemeBlue/css/style.css"
+                      ));
+            #region ticker bundless and encode
+            bundles.Add(new ScriptBundle("~/bundles/Ticker").Include(                
+                             "~/ThemeBlue/js/Ticker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ChartRender").Include(
+                             "~/ThemeBlue/js/highstock.js",
+                             "~/ThemeBlue/js/exporting.js",
+                             "~/ThemeBlue/js/Chartcp.js"
+                             ));
+            bundles.Add(new ScriptBundle("~/bundles/FollowStock").Include(
+                             "~/ThemeBlue/js/FollowStock.js"));
+            #endregion
+
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
---
+++ PhimHang.vn- layout 4/PhimHang.vn/ThemeBlue/js/FollowStock.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $(".button-flow").click(function () {
+        var stock = $('#stockHidenPage').val();
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: '/FollowStock/Create',
+            data: { stock: stock },
+            beforeSend: function (xhr) {
+                //Add your image loader here
+
+            },
+            success: function (data) {
+                if (data == "A") {
+
+                    showNotification('Đã thêm vào danh mục theo dõi');
+                    $(".button-flow").html("<i class='fa fa-minus'></i>Đang theo dõi");
+                }
+                else if (data == "R") {
+                    showNotification('Đã loại khỏi danh mục theo dõi');
+                    $(".button-flow").html("<i class='fa fa-plus'></i>Theo dõi");
+                } else {
+                    alert('Lỗi');
+                }
+            }
+        })
+    });
+});
