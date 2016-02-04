244edd3b5b75924ad2552ac51e6385f71a909e75
Nguyen Chi Hieu <n.hieu86@gmail.com>
2016-02-04 23:37:49 +0700

    add bag keyworld controller

---
+++ PhimHang.vn_BackEnd/PhimHang.vn/Controllers/BadWordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhimHang.Controllers
+{
+    public class BadWordController : Controller
+    {
+        //
+        // GET: /BadWord/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
