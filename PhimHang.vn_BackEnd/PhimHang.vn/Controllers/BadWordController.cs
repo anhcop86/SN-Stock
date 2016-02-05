fccd675ea4fc87ebde8af738a5621d3d220c0081
AnhCopNe <n.hieu86@gmail.com>
2016-02-05 09:15:10 +0700

    update

--- PhimHang.vn_BackEnd/PhimHang.vn/Controllers/BadWordController.cs
+++ PhimHang.vn_BackEnd/PhimHang.vn/Controllers/BadWordController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PhimHang.Models;
+using PagedList;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhimHang.Controllers
 {
