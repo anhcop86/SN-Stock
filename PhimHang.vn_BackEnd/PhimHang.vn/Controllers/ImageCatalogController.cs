aa9fc8ea1bd179c9ca568eee06e6057506a7acad
Nguyen Chi Hieu <n.hieu86@gmail.com>
2016-02-24 22:26:53 +0700

    back end

---
+++ PhimHang.vn_BackEnd/PhimHang.vn/Controllers/ImageCatalogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhimHang.Controllers
+{
+    public class ImageCatalogController : Controller
+    {
+        //
+        // GET: /ImageCatalog/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
